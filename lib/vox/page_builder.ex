1de1fcbe801c1a3eafe3f02f20efe94443181496
Geoffrey Lessel <code@geoffreylessel.com>
2023-04-20 21:23:53 -0700

    pass defined meta info to template files

--- lib/vox/page_builder.ex
+++ lib/vox/page_builder.ex
@@ -1,6 +1,15 @@
 defmodule Vox.PageBuilder do
   @spec build(page :: %Vox.ProcessedFile{}, template :: binary) :: binary
   def build(page, template) do
-    EEx.eval_file(template, assigns: [inner_content: page.content])
+    assigns =
+      page.bindings
+      |> Enum.filter(fn
+        {{_, EEx.Engine}, _} -> false
+        _ -> true
+      end)
+      |> Enum.into(Keyword.new())
+      |> Keyword.merge(inner_content: page.content)
+
+    EEx.eval_file(template, assigns: assigns)
   end
 end
