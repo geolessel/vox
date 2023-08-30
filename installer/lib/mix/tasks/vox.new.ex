0d1604bd32bbaf62d93b35fba10f7f28610fe1ef
Geoffrey Lessel <code@geoffreylessel.com>
2023-08-29 21:30:11 -0500

    feat: use a templater to copy template files over for vox.new

--- installer/lib/mix/tasks/vox.new.ex
+++ installer/lib/mix/tasks/vox.new.ex
@@ -2,13 +2,17 @@ defmodule Mix.Tasks.Vox.New do
   @shortdoc "Generate a new Vox application"
 
   use Mix.Task
+  use VoxNew.Templater
+
+  template("site/_root.html.eex")
+  template("site/_template.html.eex")
+  template("site/index.html.eex")
+  template("site/posts/hello-world.html.eex")
+  template("config/config.exs")
 
   @impl Mix.Task
   def run(argv) do
     {_parse, [path | _rest], _invalid} = OptionParser.parse(argv, strict: [])
-
-    # TODO better error handling
-    # if File.dir?(path), do: Mix.raise("Directory already exists: #{path}")
 
     generate(path)
   end
@@ -18,6 +22,7 @@ defmodule Mix.Tasks.Vox.New do
     |> run_mix_new()
     |> inject_deps()
     |> create_directories()
+    |> copy_templates()
   end
 
   defp run_mix_new(base_path) do
@@ -48,7 +53,20 @@ defmodule Mix.Tasks.Vox.New do
   end
 
   defp create_directories(base_path) do
-    Mix.Generator.create_directory(Path.join([base_path, "site"]))
+    @templates
+    |> Enum.map(&Path.dirname(Path.join(base_path, &1)))
+    |> Enum.uniq()
+    |> Enum.each(&Mix.Generator.create_directory/1)
+
     base_path
+  end
+
+  defp copy_templates(base_path) do
+    @templates
+    |> Enum.each(fn template ->
+      write_path = Path.join(base_path, template)
+      contents = render_template(template, app_name: String.capitalize(base_path))
+      Mix.Generator.create_file(write_path, contents)
+    end)
   end
 end
---
+++ installer/lib/vox_new/templater.ex
@@ -0,0 +1,41 @@
+defmodule VoxNew.Templater do
+  defmacro __using__(_env) do
+    quote do
+      import Mix.Generator
+      import unquote(__MODULE__)
+      Module.register_attribute(__MODULE__, :templates, accumulate: true)
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    base_path = Path.expand("../templates", __DIR__)
+
+    quote do
+      @base_templates_path unquote(base_path)
+    end
+
+    templates_ast =
+      for template_path <- Module.get_attribute(env.module, :templates) do
+        path = Path.join(base_path, template_path)
+        compiled = EEx.compile_file(path)
+
+        quote do
+          @external_resource unquote(path)
+          @file unquote(path)
+          def render_template(unquote(template_path), var!(assigns)), do: unquote(compiled)
+        end
+      end
+
+    quote do
+      unquote(templates_ast)
+      def template_files(name), do: Keyword.fetch!(@templates, name)
+    end
+  end
+
+  defmacro template(name) do
+    quote do
+      @templates unquote(name)
+    end
+  end
+end
